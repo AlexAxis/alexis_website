12409d64ca62f9aebe1a185419e34292e0dcfba8
Alexis Sousa <alexis-sousa@hotmail.com>
2020-03-26 18:04:39 +0100

    Updates

---
+++ precache-manifest.a4c1df2ea42a4ad575cbfd4681e68fa2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9c6f54e919418ab55d",
+    "url": "/alexis_website/static/css/main.73cc9b3e.chunk.css"
+  },
+  {
+    "revision": "2c9c6f54e919418ab55d",
+    "url": "/alexis_website/static/js/main.ae263c56.chunk.js"
+  },
+  {
+    "revision": "f323a60325dbeb87a3a0",
+    "url": "/alexis_website/static/js/runtime~main.3f0152b8.js"
+  },
+  {
+    "revision": "a9bf847307caf7f7574c",
+    "url": "/alexis_website/static/css/2.b8f4562c.chunk.css"
+  },
+  {
+    "revision": "a9bf847307caf7f7574c",
+    "url": "/alexis_website/static/js/2.fcb36c34.chunk.js"
+  },
+  {
+    "revision": "d07c3f951072e56e00288411496b7964",
+    "url": "/alexis_website/static/media/Alexis_Resume.d07c3f95.pdf"
+  },
+  {
+    "revision": "1a1a38bfdb33b262a33b476c4d818e72",
+    "url": "/alexis_website/static/media/alexislogo.1a1a38bf.png"
+  },
+  {
+    "revision": "bf09913a53f1c389362eed306e4b47cf",
+    "url": "/alexis_website/static/media/todoapp.bf09913a.png"
+  },
+  {
+    "revision": "1dc7f3aec5ff6aa3421667747409c0f5",
+    "url": "/alexis_website/static/media/moviesapi.1dc7f3ae.png"
+  },
+  {
+    "revision": "98f6d7d012aedb60aa66541b06fc99d7",
+    "url": "/alexis_website/static/media/my_website.98f6d7d0.png"
+  },
+  {
+    "revision": "2c400967a263f1deb35e9b9b28fb2d6b",
+    "url": "/alexis_website/static/media/wallpaper1.2c400967.jpg"
+  },
+  {
+    "revision": "84a42ba1ef219c59b53380e1b2b429a5",
+    "url": "/alexis_website/static/media/coffeecode.84a42ba1.jpg"
+  },
+  {
+    "revision": "256a1996c7b06a34202ea2b93be87f59",
+    "url": "/alexis_website/static/media/porto.256a1996.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/alexis_website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/alexis_website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/alexis_website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/alexis_website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/alexis_website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f285dd66bee2e0935e48f77514553d64",
+    "url": "/alexis_website/index.html"
+  }
+];
